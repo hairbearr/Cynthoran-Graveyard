77c61bd813b410797c5b8081b5dc9ce20bd5b89e
hairbearr <hbrock214@gmail.com>
2022-09-13 14:08:41 -0400

    Updated Project
    - Added character spritesheets
    - Drew the character in Idle_Down
    -- using DrawTexturePro()
    --- Scaled the character by 1.25, or a variable called playerScale

--- main.cpp
+++ main.cpp
@@ -11,7 +11,11 @@ int main()
     Texture2D map = LoadTexture("tilemaps/Graveyard.png");
     Vector2 mapPosition{0.0,0.0};
 
+    Texture2D player = LoadTexture("characters/Player/Idle_Down.png");
+    Vector2 playerPosition{ (float)windowWidth/2.0f - 4.0f * (0.5f * (float)player.width/6.0f), (float)windowHeight/2.0f - 4.0f * (0.5f * (float)player.height)};
+
     float playerMovementSpeed{4.0};
+    float playerScale{1.25};
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -32,7 +36,13 @@ int main()
             mapPosition = Vector2Subtract(mapPosition, Vector2Scale(Vector2Normalize(direction), playerMovementSpeed));
         }
 
+        // draw the map
         DrawTextureEx(map, mapPosition, 0.0, 1.0, WHITE);
+
+        // draw the player character
+        Rectangle source{0.f, 0.f, (float)player.width/6.f, (float)player.height};
+        Rectangle destination{playerPosition.x, playerPosition.y, playerScale * (float)player.width/6.0f, playerScale * (float)player.height};
+        DrawTexturePro(player, source, destination, Vector2{}, 0.f, WHITE);
 
         EndDrawing();
     }
