f902ded975faee566ba6afa0e3acf1bc73188dc0
hairbearr <hbrock214@gmail.com>
2022-09-13 12:52:39 -0400

    Update main.cpp
    - Added movement input
    -- used a direction vector2 and playerMovementSpeed float
    - Used the inputs to move the map around the screen, instead of moving the window around the map
    - utilized Vector Operations
    -- Vector2Scale()
    -- Vector2Normalize()
    -- Vector2Subtract()

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include "raymath.h"
 
 int main()
 {
@@ -8,14 +9,29 @@ int main()
     InitWindow(windowWidth, windowHeight, windowTitle);
 
     Texture2D map = LoadTexture("tilemaps/Graveyard.png");
+    Vector2 mapPosition{0.0,0.0};
+
+    float playerMovementSpeed{4.0};
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground(WHITE);
+        ClearBackground(BLACK);
 
-        Vector2 mapPosition{0.0,0.0};
+        Vector2 direction{};
+        if(IsKeyDown(KEY_W)) { direction.y -= 1.0; } // up
+        if(IsKeyDown(KEY_A)) { direction.x -= 1.0; } // left
+        if(IsKeyDown(KEY_S)) { direction.y += 1.0; } // down
+        if(IsKeyDown(KEY_D)) { direction.x += 1.0; } // right
+
+        if(Vector2Length(direction) != 0.0)
+        {
+            // set map position to map position minus direction
+            
+            mapPosition = Vector2Subtract(mapPosition, Vector2Scale(Vector2Normalize(direction), playerMovementSpeed));
+        }
+
         DrawTextureEx(map, mapPosition, 0.0, 1.0, WHITE);
 
         EndDrawing();
